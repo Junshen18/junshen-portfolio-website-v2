cadfb6e82b5f912ad2c3d5b090d7c1bcf4045d34
Junshen18 <junshenhere@gmail.com>
2024-09-08 15:56:20 +0800

    fixed about me mobile view

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -13,10 +13,10 @@ export default function AboutMe(){
     ]
     return(
         <>
-            <div id="about" className="h-screen w-[99%] px-12 flex justify-center">
+            <div id="about" className="h-screen  w-[99%] px-2 md:px-12 flex justify-center">
 
-                <div className="flex flex-col gap-5 justify-center w-full">
-                    <div className="justify-center flex w-full text-[80px] font-extrabold">
+                <div className="flex flex-col md:gap-5 justify-center w-full">
+                    <div className="justify-center flex w-full text-5xl md:text-[80px] font-extrabold">
                         <Reveal >
                             <div>
                                 About Me
@@ -25,7 +25,7 @@ export default function AboutMe(){
                     </div>
                     <div className=" justify-center items-center flex">
                         <Reveal >
-                            <div className="flex flex-row justify-center items-center  h-[220px] xl:h-[300px] w-full px-10">
+                            <div className="grid grid-cols-2 gap-x-0 lg:flex lg:flex-row justify-center items-center md:h-[400px] lg:h-[220px] xl:h-[300px] w-full px-10 py-3">
                                 {images.map((image, idx) => (
                                     <motion.div
                                         key={"images" + idx}
@@ -42,14 +42,14 @@ export default function AboutMe(){
                                                 rotate: 0,
                                                 zIndex: 100,
                                             }}
-                                        className="rounded-[20px] -mr-4 mt-4 p-1   bg-white  border border-neutral-100 flex-shrink-0 overflow-hidden"
+                                        className="rounded-[20px] -mr-4 mt-4 p-1 bg-white border border-neutral-100 flex-shrink-0 overflow-hidden"
                                     >
                                             <Image
                                                 src={image}
                                                 alt="images about me"
                                                 width="500"
                                                 height="320"
-                                                className="rounded-2xl w-[250px] h-[160px] xl:w-[350px] xl:h-[224px] "
+                                                className="rounded-2xl object-cover md:w-[250px] md:h-[160px] lg:w-[225px] lg:h-[144px] xl:w-[350px] xl:h-[224px] "
                                             />
                                     </motion.div>
                                 ))}
@@ -59,18 +59,15 @@ export default function AboutMe(){
                     
 
                     <Reveal>
-                    <div className="text-2xl 2xl:text-3xl text-justify px-12 2xl:px-20 flex flex-col justify-center items-center gap-5">
-                        <p>I’m currently pursuing a degree in Software Engineering at APU, 
-                        where I’m sharpening my skills in various technologies and expanding my network. 
-                        I have a strong interest in exploring a range of IT topics and cool, crazy, aesthetic stuff, 
-                        from the Web 3 industry and blockchain to fancy animation website design. 
-                        My passion for technology and problem-solving drives me to actively participate in hackathons, 
-                        where I typically take on roles as a Front End Developer, UI/UX Designer, and part-time Graphic Designer. 
-                        However, I’m not limited to frontend tasks; I also embrace backend work, showcasing my versatility 
-                        in delivering complete solutions. I love jokes and always try to add a bit of humor to life, 
+                    <div className="text-sm md:text-xl xl:text-3xl text-justify md:px-12 2xl:px-20 flex flex-col justify-center items-center gap-5">
+                        <p>I’m currently pursuing a degree in Software Engineering at APU, where I’m sharpening my skills in various technologies and expanding my network. 
+                            I'm passionate about IT topics and cool, crazy, aesthetic stuff, from the Web 3 industry and blockchain to fancy animation website design. 
+                            My passion for technology and problem-solving drives me to actively participate in hackathons, where I typically take on roles as a 
+                            Front End Developer, UI/UX Designer, and part-time Graphic Designer, while also embracing backend work to deliver complete solutions. 
+                            I love jokes and always try to add a bit of humor to life, believing that laughter makes every challenge easier to tackle!
                         believing that laughter makes every challenge easier to tackle!</p>
                         
-                        <div className="w-fit px-7 py-4 rounded-xl text-center text-4xl font-bold bg-opacity-20 bg-neutral-500">"Code is like humor. when you have to explain it it’s bad"</div>
+                        <div className="w-fit text-xl px-3 py-2 md:px-7 md:py-4 rounded-xl text-center md:text-4xl font-bold bg-opacity-20 bg-neutral-500">"Code is like humor. when you have to explain it it’s bad"</div>
                     </div>
                     
                     </Reveal>
